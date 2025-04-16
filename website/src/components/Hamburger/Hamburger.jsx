2158a25daa3f6be8b71f071a1ccbf62fd0f8a8f7
serwaapreimann <86732266+serwaapreimann@users.noreply.github.com>
2025-04-16 03:09:32 -0700

    Modals 1/2 done

--- website/src/components/Hamburger/Hamburger.jsx
+++ website/src/components/Hamburger/Hamburger.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
-import './Hamburger.scss';
 import hamburgerIcon from '../../assets/icons/music (2).svg';
+import './Hamburger.scss';
+
 export default function Hamburger (){
     const [isOpen,setIsOpen] = useState(false);
 
@@ -15,16 +16,19 @@ export default function Hamburger (){
             
 
             {isOpen && (
-                <div className='hamburger--menu'>
-                    <ul>
-                        <li>Home</li>
-                        <li>About</li>
-                        <li>Projects</li>
-                        <li>Tours</li>
-                        <li>Connect</li>
-                    </ul>
-
-
+                <div className='hamburger--overlay' onClick={toggleMenu}>
+                    <nav className='hamburger--content' onClick={(e) => e.stopPropagation()}>
+                    <button className='hamburger--close' onClick={toggleMenu} aria-label='Close'>
+                            x
+                        </button>
+                        <ul>
+                            <li><a href="">Home</a></li>
+                            <li><a href="">About</a></li>
+                            <li><a href="">Projects</a></li>
+                            <li><a href="">Events</a></li>
+                            <li><a href="">Connect</a></li>
+                        </ul>
+                    </nav>
                 </div>
             ) }
 
--- website/src/components/Hero/Hero.jsx
+++ website/src/components/Hero/Hero.jsx
@@ -1,15 +1,67 @@
+import Modal from 'react-bootstrap/Modal';
+import {useState} from 'react';
+import './Modal.scss';
+import Spotify from '../../assets/icons/spotify.webp';
+import Apple from '../../assets/icons/apple music.svg';
+import Tidal from '../../assets/icons/tidal.svg';
+import Amazon from '../../assets/icons/amazon music.svg';
+import Deezer from '../../assets/icons/deezer.svg';
+import YouTube from '../../assets/icons/yt.svg';
+import BoomPlay from '../../assets/icons/boomplay.webp';
+import BeatPort from '../../assets/icons/beatport.svg';
+import Seven from '../../assets/icons/7.webp';
+import QoBuz from '../../assets/icons/qobuz.webp';
+import Naps from '../../assets/icons/naps.webp';
 import './Hero.scss';
 
-
 export default function Hero (){
+
+    const [activeModal, setActiveModal] = useState(null);
+
     return (
         <div className='hero--container'>
             <h1>CATCH-22</h1>
             <div className='button-container'>
-                <button>LISTEN NOW</button>
-                <button>PURCHASE NOW</button>
-                <button>ALBUM INSPIRATION</button>
+                <button onClick={() => { setActiveModal('modal1')}}>LISTEN NOW</button>
+                <button onClick={()=> setActiveModal('modal2')}>PURCHASE NOW</button>
+                <button onClick={()=> setActiveModal('modal3')}>ALBUM INSPIRATION</button>
             </div>
-        </div>
+                <Modal className='modal--container' show={activeModal === 'modal1'} onHide={()=>setActiveModal(null)} centered>
+                    
+                    <Modal.Body className='modal--body'>
+                        <figure className='modal--figure'>
+                            <img src={Spotify} alt="Spotify" />
+                            <img src={Apple} alt="Apple" />
+                            <img src={Tidal} alt="Tidal" />
+                            <img src={Amazon} alt="Amazon" />
+                            <img src={Deezer} alt="Deezer" />
+                            <img src={YouTube} alt="YouTube" />
+                            <img src={Naps} alt="Napster" />
+                            <img src={BoomPlay} alt="BoomPlay" />
+                        </figure>
+                    </Modal.Body>
+                </Modal>
+
+                <Modal show={activeModal === 'modal2'} onHide={()=>setActiveModal(null)} centered>
+                    <Modal.Body className='modal--body'>
+                    <figure className='modal--figure-1'>
+                            <img src={Apple} alt="Apple" />
+                            <img src={Amazon} alt="Amazon" />
+                            <img src={QoBuz} alt="QoBuz" />
+                            <img src={BeatPort} alt="Beatport" />
+                            <img src={Seven} alt="7Digital" />
+
+                        </figure>
+                    </Modal.Body>
+                </Modal>
+
+                <Modal show={activeModal === 'modal3'} onHide={()=>setActiveModal(null)} centered>
+                    <Modal.Body className='modal--body'>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod sed quis placeat tenetur dolores pariatur earum molestiae distinctio a, architecto autem dolorem ab saepe nostrum ipsam repellendus nihil rerum culpa.
+                    </p>
+                    </Modal.Body>
+                </Modal>
+            </div>
     )
 }
